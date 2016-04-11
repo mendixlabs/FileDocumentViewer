31ec5ef48cb5d090e812ce4c7f85959dc42dd264
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-04-11 16:31:54 +0200

    Code cleanup

--- src/FileDocumentViewer/widget/FileDocumentViewer.js
+++ src/FileDocumentViewer/widget/FileDocumentViewer.js
@@ -1,105 +1,99 @@
-/*jslint white:true, nomen: true, plusplus: true */
-/*global mx, require */
-/*mendix */
-
-// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 require( [
-    'require', // require.toUrl
-    'dojo/_base/declare',
-    'mxui/widget/_WidgetBase',
-    'dijit/_TemplatedMixin',
-    'mxui/dom',
-    'dojo/dom',
-    'dojo/query',
-    'dojo/dom-class',
-    'dojo/dom-attr',
-    'dojo/dom-construct',
-    'dojo/dom-style',
-    'dojo/_base/lang',
-    'dojo/text',
-    'dojo/text!FileDocumentViewer/widget/templates/FileDocumentViewer.html'
+    "require",
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/query",
+    "dojo/dom-class",
+    "dojo/dom-attr",
+    "dojo/dom-construct",
+    "dojo/dom-style",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/text!FileDocumentViewer/widget/templates/FileDocumentViewer.html"
 ], function (require, declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domClass, domAttr, domConstruct, domStyle, lang, text, widgetTemplate) {
-    'use strict';
+    "use strict";
 
-    // Declare widget's prototype.
-    return declare('FileDocumentViewer.widget.FileDocumentViewer', [ _WidgetBase, _TemplatedMixin ], {
-        // _TemplatedMixin will create our dom node using this HTML template.
+    return declare("FileDocumentViewer.widget.FileDocumentViewer", [ _WidgetBase, _TemplatedMixin ], {
+
         templateString: widgetTemplate,
 
         // Parameters configured in the Modeler.
-        mfToExecute: '',
-        messageString: '',
-        backgroundColor: '',
+        mfToExecute: "",
+        messageString: "",
+        backgroundColor: "",
         usePDFjs: false,
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        // Internal variables.
         _handle: null,
         _contextObj: null,
         _objProperty: null,
         iframeNode:null,
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function () {
-            console.log(this);
+            logger.debug(this.id + ".postCreate");
+
             if (!this.showheader) {
-                domStyle.set(this.toolbarNode, 'display', 'none');
-                domClass.add(this.groupboxBody, 'no-header');
+                domStyle.set(this.toolbarNode, "display", "none");
+                domClass.add(this.groupboxBody, "no-header");
             }
 
             domStyle.set(this.domNode, {
-                'width': this.width === 0 ? 'auto' : this.width + 'px'
+                "width": this.width === 0 ? "auto" : this.width + "px"
             });
 
             this._setupEvents();
         },
 
-        _iframeNodeCreate: function() {
-            this.iframeNode = domConstruct.create('iframe', null, this.groupboxBody);
-            domAttr.set(this.iframeNode, 'id', 'iframeNode');
-            domAttr.set(this.iframeNode, 'class', 'documentiframe');
+        _iframeNodeCreate: function () {
+            logger.debug(this.id + "._iframeNodeCreate");
+            this.iframeNode = domConstruct.create("iframe", null, this.groupboxBody);
+            domAttr.set(this.iframeNode, "id", "iframeNode");
+            domAttr.set(this.iframeNode, "class", "documentiframe");
 
             domStyle.set(this.iframeNode, {
-                'height': this.height === 0 ? 'auto' : this.height + 'px',
-                'width' : '100%',
-                'border-width': 0
+                "height": this.height === 0 ? "auto" : this.height + "px",
+                "width" : "100%",
+                "border-width": 0
             });
         },
 
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
-            console.log(obj);
+            logger.debug(this.id + ".update");
             this._contextObj = obj;
             this._resetSubscriptions();
-            this._updateRendering();
-
-            callback();
+            this._updateRendering(callback);
         },
 
-        _updateRendering: function () {
-
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
             domConstruct.destroy(this.iframeNode);
             this.iframeNode = null;
             this._iframeNodeCreate();
 
-            if (this._contextObj && this._contextObj.get('HasContents'))  {
+            if (this._contextObj && this._contextObj.get("HasContents"))  {
                 if (this.usePDFjs/* && this._contextObj.get("Name").indexOf(".pdf") !== -1*/) {
-                    var pdfJSViewer = require.toUrl('FileDocumentViewer/lib/pdfjs/web/viewer.html').split("?")[0],
+                    var pdfJSViewer = require.toUrl("FileDocumentViewer/lib/pdfjs/web/viewer.html").split("?")[0],
                         encoded = pdfJSViewer + "?file=" + encodeURIComponent(mx.appUrl + this._getFileUrl());
 
-                    domAttr.set(this.iframeNode, 'src', encoded);
+                    domAttr.set(this.iframeNode, "src", encoded);
                 } else {
-                    domAttr.set(this.iframeNode, 'src', this._getFileUrl());
+                    domAttr.set(this.iframeNode, "src", this._getFileUrl());
                 }
 
-                domAttr.set(this.headerTextNode, 'innerHTML',this._contextObj.get(this.headertitle));
+                domAttr.set(this.headerTextNode, "innerHTML",this._contextObj.get(this.headertitle));
             } else {
-                domAttr.set(this.iframeNode, 'src', require.toUrl('FileDocumentViewer/widget/ui/blank.html'))
-                domAttr.set(this.headerTextNode, 'innerHTML','...');
+                domAttr.set(this.iframeNode, "src", require.toUrl("FileDocumentViewer/widget/ui/blank.html"));
+                domAttr.set(this.headerTextNode, "innerHTML","...");
             }
+
+            mendix.lang.nullExec(callback);
         },
 
         _resetSubscriptions: function () {
-            // Release handle on previous object, if any.
+            logger.debug(this.id + "._resetSubscriptions");
             if (this._handle) {
                 this.unsubscribe(this._handle);
                 this._handle = null;
@@ -113,28 +107,30 @@ require( [
             }
         },
 
-        _setupEvents : function(){
-            this.connect(this.enlargeNode, 'onclick', this._eventEnlarge);
-            this.connect(this.popoutNode, 'onclick',  this._eventPopout);
+        _setupEvents : function () {
+            logger.debug(this.id + "._setupEvents");
+            this.connect(this.enlargeNode, "onclick", this._eventEnlarge);
+            this.connect(this.popoutNode, "onclick",  this._eventPopout);
         },
 
-        _getFileUrl : function() {
-            var url;
-            if (this._contextObj === null || this._contextObj.get('Name') === null) {
-                url = require.toUrl('FileDocumentViewer/widget/ui/error.html');
+        _getFileUrl : function () {
+            logger.debug(this.id + "._getFileUrl");
+            if (this._contextObj === null || this._contextObj.get("Name") === null) {
+                return require.toUrl("FileDocumentViewer/widget/ui/error.html");
             } else {
-                url = 'file?target=window&guid=' + this._contextObj.getGuid() + '&csrfToken=' + mx.session.getCSRFToken() + '&time=' + Date.now();
+                return "file?target=window&guid=" + this._contextObj.getGuid() + "&csrfToken=" + mx.session.getCSRFToken() + "&time=" + Date.now();
             }
-            return url;
         },
 
-        _eventEnlarge : function() {
+        _eventEnlarge : function () {
+            logger.debug(this.id + "._eventEnlarge");
             domStyle.set(this.iframeNode, {
-                height: (domStyle.get(this.iframeNode, 'height') * 1.5) +'px'
+                height: (domStyle.get(this.iframeNode, "height") * 1.5) +"px"
             });
         },
 
-        _eventPopout : function() {
+        _eventPopout : function () {
+            logger.debug(this.id + "._eventPopout");
             window.open(this._getFileUrl());
         }
     });
